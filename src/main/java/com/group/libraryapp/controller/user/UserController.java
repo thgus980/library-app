cd0d0de3a1375390b5d363a00f7a3047cfac3307
thgus980 <thgus980@naver.com>
2025-08-02 23:15:36 +0900

    20250802 학습내용

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,30 +3,44 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users=new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate; //jdbc 템플릿을 이용해서 db에 접근 가능한 것
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate=jdbcTemplate;
+    }
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql= "INSERT INTO user (name, age) VALUES (?, ?)"; // 이름과 나이는 고정된 sql이 아니기 때문에 유동적으로 값을 넣어줄 수 있도록
+        jdbcTemplate.update(sql, request.getName(), request.getAge()); // String sql의 물음표에 각각 request.getName(), request.getAge() 넣어짐
     }
 
     @GetMapping("/user") // GET /user
     public List<UserResponse> getUsers () {
-        List<UserResponse> responses=new ArrayList<>();
-        for (int i=0; i< users.size(); i++){
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-        return responses;
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() { //select * from user(String sql)를 수행하여 나온 결과들을 우리가 작성한 로직에 따라서 UserResponse로 바꿔주는 작업
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age= rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
     }
 }
